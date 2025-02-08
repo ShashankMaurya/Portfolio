550219ffe8f2d4b73fd7372112651e526c9bb493
ShashankMaurya <shashankmaurya260101@gmail.com>
2025-02-08 19:47:13 +0530

    Updated Resume Link and added age auto-update

--- index.js
+++ index.js
@@ -15,9 +15,23 @@ function trigger(){
     }
 }
 
+function getAge(birthdate){
+    var today = new Date();
+    var birthDate = new Date(birthdate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())){
+        age--;
+    }
+    return age;
+}
+
 
 function animate(){
     setTimeout(trigger, 1200);
 }
 
+const birthDate = '2001-01-26'
+
+document.querySelector('div#age div.value').textContent= getAge(birthDate);
 window.addEventListener('scroll', animate);
